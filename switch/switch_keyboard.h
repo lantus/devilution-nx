b7625d4f857ba7e1642ce38856229868ed84b390
rsn8887 <rsn8887@users.noreply.github.com>
2019-07-11 11:32:00 -0500

    Add virtual keyboard support on Switch

---
+++ switch/switch_keyboard.h
@@ -0,0 +1,5 @@
+#ifndef SWITCH_KEYBOARD_H
+#define SWITCH_KEYBOARD_H
+
+void switch_start_text_input(char *guide_text, char *initial_text, int multiline);
+#endif /* OPENRCT2_UI_SWITCH_KEYBOARD_H */
